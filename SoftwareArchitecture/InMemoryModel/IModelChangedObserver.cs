de26e8e1f4ab904143e8cec751078f07c0ce5059
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-12-13 19:19:36 +0300

    Создания папок и двух инрефейсов и частичное заполнения по диаграме.

---
+++ SoftwareArchitecture/InMemoryModel/IModelChangedObserver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareArchitecture.InMemoryModel
+{
+    /// <summary>
+    /// Наблюдатель с измененной моделью
+    /// </summary>
+    internal interface IModelChangedObserver
+    {
+        /// <summary>
+        /// Применить модель обновления
+        /// </summary>
+        public void ApplyUpdateModel();
+    }
+}
---
+++ SoftwareArchitecture/InMemoryModel/IModelChanger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareArchitecture.InMemoryModel
+{
+    /// <summary>
+    /// Средство смены моделей
+    /// </summary>
+    internal interface IModelChanger
+    {
+        /// <summary>
+        /// Изменение уведомления
+        /// </summary>
+        /// <param name="sender"></param>
+        public void NotifyChange(IModelChanger sender);
+    }
+}
