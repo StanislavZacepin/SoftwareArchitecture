9007b8e5f9c0d760600bfd5dc464954ac4c63ce6
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-12-24 17:51:07 +0300

    Рефакторинг

--- SoftwareArchitecture/Program.cs
+++ SoftwareArchitecture/Program.cs
@@ -3,7 +3,8 @@ using SoftwareArchitecture.Dz2.Entity.People;
 using SoftwareArchitecture.Dz2.Interfaces;
 using SoftwareArchitecture.Dz2.Model;
 
-Random random = new Random();
+#region Dz2
+/*Random random = new Random();
 List<ItemGenerator> generatorList = new();
 
 generatorList.Add(new GemGenerator());
@@ -16,14 +17,15 @@ generatorList.Add(new TreeGenerator());
 Human humanVlad = new Human { Name = "vlad", LastName = "vladch", Age = 15, Height = 200 };
 for (int i = 0; i < 20; i++)
 {
-    int index = Math.Abs(random.Next(1,7));
+    int index = Math.Abs(random.Next(1, 7));
     ItemGenerator itemGenerator = generatorList[index];
     humanVlad.inventory.Add(itemGenerator);
     itemGenerator.openReward();
-    
+
 
 }
 
 
 humanVlad.speak();
-
+*/
+#endregion
